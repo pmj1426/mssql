df188203575d057bc9cb6d4f810992e763aacb26
Brandon <bmw7216@rit.edu>
2026-02-22 13:15:23 -0500

    codex fixes

--- main.go
+++ main.go
@@ -4,22 +4,23 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"net/url"
+	"strings"
 
-	mssql "github.com/denisenkom/go-mssqldb"
-	"github.com/jcmturner/gokrb5/v8/client"
+	mssql "github.com/microsoft/go-mssqldb"
+	_ "github.com/microsoft/go-mssqldb/integratedauth/krb5"
 	"github.com/scorify/schema"
 )
 
 type Schema struct {
-	Server    string `key:"target"`
-	Port      int    `key:"port" default:"3306"`
-	KDCServer string `key:"kdcserver"`
-	//KDCPort   int    `key:"kdcport" default:"88"`
-	Domain   string `key:"domain"`
-	Username string `key:"username"`
-	Password string `key:"password"`
-	Database string `key:"database"`
-	Query    string `key:"query"`
+	Server         string `key:"target"`
+	Port           int    `key:"port" default:"1433"`
+	Domain         string `key:"domain"`
+	Username       string `key:"username"`
+	Password       string `key:"password"`
+	Database       string `key:"database"`
+	Krb5ConfigFile string `key:"krb5_config_file"`
+	Query          string `key:"query"`
 }
 
 func Validate(config string) error {
@@ -54,13 +55,9 @@ func Validate(config string) error {
 		return fmt.Errorf("domain is required; got %q", conf.Domain)
 	}
 
-	if conf.KDCServer == "" {
-		return fmt.Errorf("KDC Server is required; got %q", conf.KDCServer)
+	if conf.Krb5ConfigFile == "" {
+		return fmt.Errorf("krb5_config_file is required; got %q", conf.Krb5ConfigFile)
 	}
-
-	//if conf.KDCPort <= 0 || conf.KDCPort > 65535 {
-	//	return fmt.Errorf("port is invalid; got %d", conf.KDCPort)
-	//}
 
 	return nil
 }
@@ -78,57 +75,54 @@ func Run(ctx context.Context, config string) error {
 		return fmt.Errorf("context deadline is not set")
 	}
 
-	krbConf := fmt.Sprintf(`
-[libdefaults]
- default_realm = %s
- dns_lookup_kdc = false
-[realms]
- %s = { kdc = %s }
-`, conf.Domain, conf.Domain, conf.KDCServer)
+	u := &url.URL{
+		Scheme: "sqlserver",
+		Host:   fmt.Sprintf("%s:%d", conf.Server, conf.Port),
+		User:   url.UserPassword(conf.Username, conf.Password),
+	}
 
-	cfg, err := config.NewFromString(krbConf)
+	query := u.Query()
+	query.Set("database", conf.Database)
+	query.Set("authenticator", "krb5")
+	query.Set("krb5-configfile", conf.Krb5ConfigFile)
+	if conf.Domain != "" && !strings.Contains(conf.Username, "@") {
+		query.Set("krb5-realm", conf.Domain)
+	}
+	u.RawQuery = query.Encode()
+
+	connector, err := mssql.NewConnector(u.String())
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to build mssql connector: %w", err)
 	}
 
-	cl := client.NewWithPassword(conf.Username, conf.Domain, conf.Password, cfg)
-	if err := cl.Login(); err != nil {
-		return fmt.Errorf("Kerberos login failed: %v", err)
-	}
-
-	conn, err := mssql.NewKerberosConnector(
-		fmt.Sprintf("sqlserver://%s:%d?database=master", conf.Server, conf.Port),
-		cl,
-	)
-
-	db := sql.OpenDB(conn)
+	db := sql.OpenDB(connector)
 	defer db.Close()
 
-	ctx, cancel := context.WithDeadline(
-		context.Background(),
-		deadline,
-	)
+	execCtx, cancel := context.WithDeadline(context.Background(), deadline)
 	defer cancel()
 
-	conn.SetMaxIdleConns(-1)
-	conn.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(0)
+	db.SetMaxOpenConns(1)
 
-	err = conn.PingContext(ctx)
+	err = db.PingContext(execCtx)
 	if err != nil {
 		return fmt.Errorf("failed to ping mssql server: %w", err)
 	}
 
 	if conf.Query != "" {
-		rows, err := conn.QueryContext(ctx, conf.Query)
+		rows, err := db.QueryContext(execCtx, conf.Query)
 		if err != nil {
 			return fmt.Errorf("failed to execute query: %w", err)
 		}
 		defer rows.Close()
 
 		if !rows.Next() {
+			if err := rows.Err(); err != nil {
+				return fmt.Errorf("query failed while reading rows: %w", err)
+			}
 			return fmt.Errorf("no rows returned from query: %q", conf.Query)
 		}
 	}
-	defer cl.Destroy()
+
 	return nil
 }
